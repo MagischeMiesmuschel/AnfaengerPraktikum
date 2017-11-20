fe38fc477e17939fb8bd78a9344f99f56a220511
Kevin Talits <kevin.talits@tu-dortmund.de>
2017-11-20 20:52:19 +0100

    Auswertung und plot.py fertig.

--- V354/plot.py
+++ V354/plot.py
@@ -11,14 +11,20 @@ def T_a(t1,t2):
 def U_a(U1,U2):
     return U2-U1
 
+def e_fkt(t,A,B):
+    return (A * np.exp(-B * t))
+
+def hw(E,F):
+    return E
 
 # T_a/U_a ist die Steigung der einhüllenden Kurve
 
-t_a = [5, 35, 64, 94, 123, 152, 182, 212, 241, 270, 300, 330, 359, 388] #in Mikro Sekunden
-Spannung_a = [235, 217, 202, 188, 177, 166, 160, 152, 147, 142, 138, 135, 132, 130] #in Volt
-Frequenz_cd = [15, 20, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 45, 50] #in Kilo Herz
-Uc_c = [130, 146, 178, 189, 200, 214, 229, 247, 264, 279, 287, 285, 272, 253, 230, 210, 192, 176, 126, 102] #in V
-delta_t_d = [1.2, 1.6, 2.4, 2.8, 3.2, 3.6, 3.8, 4.4, 5.0, 5.6, 6.8, 7.6, 8.4, 8.8, 9.4, 9.6, 10, 10, 9.8, 9.2] #in Mikro Sekunden
+t_a, Spannung_a = np.genfromtxt('werteA.txt', unpack = True)
+Frequenz_cd, Uc_c = np.genfromtxt('werteC.txt', unpack = True)
+Frequenz_cd, delta_t_d = np.genfromtxt('werteD.txt', unpack =True)
+t_a = t_a * 1e-6
+delta_t_d = delta_t_d * 1e-6
+Frequenz_cd = Frequenz_cd * 1e3
 
 #Werte von Gerät 2
 L = ufloat(10.11e-3, 0.03e-3) #in Henry
@@ -29,22 +35,95 @@ R_ap = 3.28e3 #in Ohm
 U_0 = 117 #in Volt
 R_N = 50 #in Ohm    Der Wiederstand des Netzgeräts
 w_0 = (1/(L * C))**(0.5) #Omega_Null
-f_res = 1/(2 * math.pi) * ((1/(L * C)) - ((R2+R_N)**2)/2 * (L**2))**(0.5) # errechnete Resonanz-Frequenz
-print(f_res) 
-plt.plot(t_a, Spannung_a)
+f_res = (1/(2 * math.pi)) * ((1/(L * C)) - ((R2+R_N)**2)/(4 * (L**2)))**(0.5) # errechnete Resonanz-Frequenz
+#Teil a)
+
+plt.plot(t_a, Spannung_a, label = 'Messdaten')
 plt.xlabel('t in $\mu s$')
 plt.ylabel('U in $V$')
-plt.savefig('plot1.pdf')
+#plt.xticks([0, 0 /  1,5e-6/ 35e-6/ 64e-6/ 94e-6/ 123e-6/ 152e-6/ 182e-6/ 212e-6/ 241e-6/ 270e-6/ 300e-6/ 330e-6],
+ #           [0, 5, 35, 54, 94, 123, 152, 182, 212, 241,270,300, 330])
+params, cov_matrix = curve_fit(e_fkt, t_a, Spannung_a)
+errors = np.sqrt(np.diag(cov_matrix))
+print('A = ', params[0], '+/-', errors[0])  
+print('B = ', params[1], '+/-', errors[1])  #mu
+plt.plot(t_a, e_fkt(t_a, *params), 'r-', label = 'Exp. Fit')
+plt.legend()
+plt.savefig('build\plot1.pdf')
+mu = ufloat(params[1], errors[1]) / (2 * math.pi)           #mu
+plt.clf()
+
+T_ex_versuch = 1 / (2 * math.pi * mu)
+T_ex_theorie = 2 * L / (R2+R_N)
+T_ex_prozent = T_ex_versuch / T_ex_theorie
+
+print('T_ex_versuch = ', T_ex_versuch,'T_ex_theorie = ', T_ex_theorie, 'T_ex_prozent = ', T_ex_prozent)
+
+R_eff = 4 * math.pi * mu * L
+print('R_eff = ',R_eff)
+print('mu = ', mu)
+
+#Teil c)
 
 R_ap_rechn=(4 * L / C)**(0.5)
 R_ap_prozent = R_ap / R_ap_rechn
-print(R_ap_prozent) #Prozentuale Abweichung vom Gemessenen zum Errechneten Wert
+print('R_ap = ', R_ap, 'R_ap_rechn = ', R_ap_rechn, 'R_ap_prozent = ', R_ap_prozent) #Prozentuale Abweichung vom Gemessenen zum Errechneten Wert
 U_diff = [x / U_0 for x in Uc_c]
-plt.plot(Frequenz_cd, U_diff)
-plt.xlabel('F in $kHz$')
+plt.plot(Frequenz_cd, U_diff, 'k.', label = 'Messdaten')
+plt.xlabel('f in $kHz$')
 plt.ylabel('$U$ / $U_0$')
-plt.savefig('plot2.pdf')
-q = 1 / (w_0 * C * R2) #Resonanzüberhöhung, Güte
-print(q)
+plt.legend()
+plt.savefig('build\plot2.pdf')
+q_rechn = 1 / (w_0 * C * (R2+R_N)) #Resonanzüberhöhung, Güte
+
+plt.clf()
+print('U_diff         Frequenz_cd')
 for x in range (0, 19):
-    print(U_diff[x])
+    print(U_diff[x], Frequenz_cd[x])
+
+f_res_gemessen = 33 #in kHz, Frequenz zum Maximum aus Plot2
+q_gemessen = 2.45 #Maximum vom Plot2
+q_halbw = 1/(np.sqrt(2)) * q_gemessen
+q_prozent = q_gemessen / q_rechn
+print('q_rechn = ', q_rechn, 'q_gemessen = ',q_gemessen, 'q_prozent = ', q_prozent )
+print('q_halbw = ', q_halbw)
+plt.plot(Frequenz_cd, U_diff, label = 'Messdaten')
+plt.xlabel('f in $kHz$')
+plt.ylabel('$U$ / $U_0$')
+plt.xlim(25000, 40000)
+plt.ylim(1.4, 2.5)
+q_array = np.genfromtxt('q_halbw.txt', unpack = True)
+plt.plot(Frequenz_cd, q_array, 'r--')
+f_array = np.genfromtxt('f_halbw.txt', unpack = True) #27.20 kHz
+f_array2 = np.genfromtxt('f_halbw2.txt', unpack = True) #38.34 kHz
+y_gerade = np.genfromtxt('y_werte.txt', unpack = True)
+plt.plot(f_array,y_gerade,'r--', label = 'Halbwertsbreite')
+plt.plot(f_array2,y_gerade,'r--')
+plt.legend()
+
+plt.savefig('build\plot3.pdf')
+
+plt.clf()
+
+nu1 = -(R2+R_N)/(2*L) + ((R2+R_N)**2/(4 * L**2) + 1/(L * C))**(0.5)
+nu2 = (R2+R_N)/(2*L) + ((R2+R_N)**2/(4 * L**2) + 1/(L * C))**(0.5)
+
+print('nu1 = ', nu1, 'nu2 = ', nu2)
+
+
+#Teil d)
+
+phi = delta_t_d * Frequenz_cd *2 * math.pi
+
+print('phi = ', phi)
+
+f_array_res = np.genfromtxt('f_res.txt', unpack = True)
+
+plt.plot(Frequenz_cd, phi, 'k.', label = 'Messdaten')
+plt.xlabel('f in $kHz$')
+plt.ylabel('$U$ / $U_0$')
+plt.plot(f_array,y_gerade,'r--', label = r'$\nu_+$ bzw. $\nu_-$')
+plt.plot(f_array2,y_gerade,'r--')
+plt.plot(f_array_res, y_gerade, 'g--', label = 'Resonanzfrequenz')
+plt.legend()
+plt.savefig('build\plot4.pdf')
