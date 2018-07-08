e6ef0258cfcd83804720417149c2ee0bd2fe38b0
Jannis Speer <jannis.speer@tu-dortmund.de>
2018-07-08 17:10:01 +0200

    plots für V406

--- V406/plot.py
+++ V406/plot.py
@@ -1,95 +1,106 @@
-import matplotlib.pyplot as plt
 import numpy as np
-from scipy.optimize import curve_fit
-from uncertainties import ufloat
-import scipy.constants as scicon
-import math
+import scipy.optimize
+import uncertainties as unc
+import uncertainties.unumpy as unp
+from scipy import optimize
+import matplotlib.pyplot as plt
 from uncertainties import ufloat
 from uncertainties import correlated_values
-from scipy import optimize
 
 def theory(zeta, A0, zeta_0, b):
-    return (A0 * b * np.sinc(b * np.sin((zeta - zeta_0)/(1) / 635e-9))**2)
+    return (A0 * b * np.sinc(b * np.sin(zeta - zeta_0) / 635e-9))**2
 
-def einzel(x, A, B, x0):
-    return A * (np.sin((math.pi / (635e-9)) * B * np.sin(x-x0))/((math.pi / (635e-9)) * B * np.sin(x-x0)))**2
-
-def f(x,a,b):
-    return a**2 * b * (635e-9 / (math.pi * b * np.sin(x)))**2 * (np.sin((math.pi * b * np.sin(x))/ 635e-9))**2
-
-def g1(x,a,b):
-    return a**2 * 2 * (np.cos((math.pi * 0.25e-3 * np.sin(x)) / 635e-9))**2 * (635e-9 / (math.pi * b * np.sin(x)))**2 * (np.sin((math.pi * b * np.sin(x))/ 635e-9))**2
-
-def g2(x,a,b):
-    return a**2 * 2 * (np.cos((math.pi * 0.35e-3 * np.sin(x)) / 635e-9))**2 * (635e-9 / (math.pi * b * np.sin(x)))**2 * (np.sin((math.pi * b * np.sin(x))/ 635e-9))**2
+def theory2(zeta, A0, zeta_0, b, d):
+    return (2 * np.cos(np.pi*d*np.sin(zeta - zeta_0) / 635e-9))**2*theory(zeta, A0, zeta_0, b)
 
 I_dunkel = 0.85e-9 #Dunkelstrom
 b = 0.15e-3 #Breite pro Spalt
 L = 1 #Abstand von Spalt zu Detektor
 
 I_doppel1, x_doppel1 = np.genfromtxt("data/werte1.txt", unpack=True) #Doppelspalt1
+x_doppel1 *= 1e-3 #s/m
+I_doppel1 *= 1e-6 #I/A
 a1 = 0.25e-3 #Abstand 1 der Spalte
-#I_doppel1 = I_doppel1 - I_dunkel
+I_doppel1 = I_doppel1 - I_dunkel
 
 x_doppel2, I_doppel2 = np.genfromtxt("data/werte2.txt", unpack=True) #Doppelspalt2
+x_doppel2 *= 1e-3 #s/m
+I_doppel2 *= 1e-6 #I/A
 a2 = 0.35e-3 #Abstand 1 der Spalte
-#I_doppel1 = I_doppel2 - I_dunkel
+I_doppel2 = I_doppel2 - I_dunkel
 
 x_einzel, I_einzel = np.genfromtxt("data/werte3.txt", unpack=True) #Einzelspalt
-#I_einzel = I_einzel -I_dunkel
+x_einzel *= 1e-3 #s/m
+I_einzel *= 1e-6 #I/A
+I_einzel = I_einzel -I_dunkel
 
-phi_einzel = x_einzel - 20.99   #Berechnung von Phi mit x_0 wo das Max ist
-phi_doppel1 = x_doppel1 - 23.1
-phi_doppel2 = x_doppel2 - 22.75
+x_fit1 = np.linspace(0.009, 0.033, 1000)
+x_fit2 = np.linspace(0.015, 0.031, 1000)
+x_fit3 = np.linspace(0.019, 0.0264, 1000)
 
-x = np.linspace(-11e-3,11e-3,1000)
+############################################################
 
-x_einzel *= 1e-3
-I_einzel *= 1e-6
+params1, covariance_matrix1 = optimize.curve_fit(theory, x_einzel, I_einzel, p0=[10, 0.02, 1e-3])
 
-params1, pcov1 = curve_fit(einzel,x_einzel,I_einzel)
+A_0, x_0, b = correlated_values(params1, covariance_matrix1)
 
-print("A0 Einzelspalt: ", params1[0], "+/-", pcov1[0][0])
-print("Spaltbreite Einzelspalt: ", params1[1], "+/-", pcov1[1][1])
-print("x0 Einzelspalt: ", params1[2], "+/-", pcov1[2][2])
+print('Jetzt kommt der erste Einzelspalt')
+print('A_0 =', A_0)
+print('x_0 =', x_0)
+print('b =', b)
 
-
-plt.plot(x, einzel(x, *params1), 'b', label="Fit")
-
-plt.plot(x_einzel-21e-3, I_einzel, 'rx', label="Messwerte")
+plt.plot(x_fit1*1e3, theory(x_fit1, *params1)*1e6, 'b', label="Fit")
+plt.plot(x_einzel*1e3, I_einzel*1e6, 'rx', label="Messwerte")
 plt.grid()
 plt.legend()
-plt.xlabel("Position in mm")
-plt.ylabel("Intensität in µA")
+plt.xlim(9, 33)
+plt.xlabel(r'Position in mm')
+plt.ylabel(r'Intensität in $\si{\micro}$A')
 plt.savefig("build/plot1.pdf")
 plt.clf()
 
-params2, pcov2 = curve_fit(g1,phi_doppel1,I_doppel1)
+############################################################
 
-print("A0 Doppelspalt1: ", params2[0], "+/-", pcov2[0][0])
-print("Spaltbreite Einzelspalt: ", params2[1], "+/-", pcov2[1][1])
+params2, covariance_matrix2 = optimize.curve_fit(theory2, x_doppel1, I_doppel1, p0=[10, 0.0231, 0.15e-3, 0.25e-3])
 
-plt.plot(phi_doppel1, g1(phi_doppel1, *params2), 'b', label="Fit")
+A_0, x_0, b, d = correlated_values(params2, covariance_matrix2)
 
-plt.plot(phi_doppel1, I_doppel1, 'rx', label="Messwerte")
+print('Jetzt kommt der Doppelspalt 1')
+print('A_0 =', A_0)
+print('x_0 =', x_0)
+print('b =', b)
+print('d =', d)
+
+plt.plot(x_fit2*1e3, theory2(x_fit2, *params2)*1e6, 'b', label="Fit")
+plt.plot(x_doppel1*1e3, I_doppel1*1e6, 'rx', label="Messwerte")
 plt.grid()
 plt.legend()
-plt.xlabel("Position in mm")
-plt.ylabel("Intensität in µA")
+plt.xlim(15, 31)
+plt.xlabel(r'Position in mm')
+plt.ylabel(r'Intensität in $\si{\micro}$A')
 plt.savefig("build/plot2.pdf")
 plt.clf()
 
-params3, pcov3 = curve_fit(g2,phi_doppel2,I_doppel2)
+############################################################
 
-print("A0 Doppelspalt2: ", params3[0], "+/-", pcov3[0][0])
-print("Spaltbreite Einzelspalt: ", params3[1], "+/-", pcov3[1][1])
+params3, covariance_matrix3 = optimize.curve_fit(theory2, x_doppel2, I_doppel2, p0=[10, 0.0225, 0.15e-3, 0.35e-3])
 
-plt.plot(phi_doppel2, g2(phi_doppel2, *params3), 'b', label="Fit")
+A_0, x_0, b, d = correlated_values(params3, covariance_matrix3)
 
-plt.plot(phi_doppel2, I_doppel2, 'rx', label="Messwerte")
+print('Jetzt kommt der Doppelspalt 2')
+print('A_0 =', A_0)
+print('x_0 =', x_0)
+print('b =', b)
+print('d =', d)
+
+plt.plot(x_fit3*1e3, theory2(x_fit3, *params3)*1e6, 'b', label="Fit")
+plt.plot(x_doppel2*1e3, I_doppel2*1e6, 'rx', label="Messwerte")
 plt.grid()
 plt.legend()
-plt.xlabel("Position in mm")
-plt.ylabel("Intensität in µA")
+plt.xlim(19, 26.5)
+plt.xlabel(r'Position in mm')
+plt.ylabel(r'Intensität in $\si{\micro}$A')
 plt.savefig("build/plot3.pdf")
 plt.clf()
+
+############################################################
