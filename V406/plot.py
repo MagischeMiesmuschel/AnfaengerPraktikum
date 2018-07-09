4bea6e7e6025c49f427a543147c4b365f3fc07ee
Kevin Talits <kevin.talits@tu-dortmund.de>
2018-07-09 08:11:09 +0200

    Auswertung 406

--- V406/plot.py
+++ V406/plot.py
@@ -18,18 +18,27 @@ b = 0.15e-3 #Breite pro Spalt
 L = 1 #Abstand von Spalt zu Detektor
 
 I_doppel1, x_doppel1 = np.genfromtxt("data/werte1.txt", unpack=True) #Doppelspalt1
+print('Tabelle Doppel1:')
+for i in range(60):
+    print(x_doppel1[i], " & " ,I_doppel1[i], "\\\\")
 x_doppel1 *= 1e-3 #s/m
 I_doppel1 *= 1e-6 #I/A
 a1 = 0.25e-3 #Abstand 1 der Spalte
 I_doppel1 = I_doppel1 - I_dunkel
 
 x_doppel2, I_doppel2 = np.genfromtxt("data/werte2.txt", unpack=True) #Doppelspalt2
+print('Tabelle Doppel2:')
+for i in range(46):
+    print(x_doppel2[i], " & " ,I_doppel2[i], "\\\\")
 x_doppel2 *= 1e-3 #s/m
 I_doppel2 *= 1e-6 #I/A
 a2 = 0.35e-3 #Abstand 1 der Spalte
 I_doppel2 = I_doppel2 - I_dunkel
 
 x_einzel, I_einzel = np.genfromtxt("data/werte3.txt", unpack=True) #Einzelspalt
+print('Tabelle Einzelspalt:')
+for i in range(42):
+    print(x_einzel[i], " & " ,I_einzel[i], "\\\\")
 x_einzel *= 1e-3 #s/m
 I_einzel *= 1e-6 #I/A
 I_einzel = I_einzel -I_dunkel
@@ -37,6 +46,8 @@ I_einzel = I_einzel -I_dunkel
 x_fit1 = np.linspace(0.009, 0.033, 1000)
 x_fit2 = np.linspace(0.015, 0.031, 1000)
 x_fit3 = np.linspace(0.019, 0.0264, 1000)
+
+
 
 ############################################################
 
@@ -55,7 +66,7 @@ plt.grid()
 plt.legend()
 plt.xlim(9, 33)
 plt.xlabel(r'Position in mm')
-plt.ylabel(r'Intensität in $\si{\micro}$A')
+plt.ylabel(r'Intensität in µA')
 plt.savefig("build/plot1.pdf")
 plt.clf()
 
@@ -77,7 +88,7 @@ plt.grid()
 plt.legend()
 plt.xlim(15, 31)
 plt.xlabel(r'Position in mm')
-plt.ylabel(r'Intensität in $\si{\micro}$A')
+plt.ylabel(r'Intensität in µA')
 plt.savefig("build/plot2.pdf")
 plt.clf()
 
@@ -99,8 +110,19 @@ plt.grid()
 plt.legend()
 plt.xlim(19, 26.5)
 plt.xlabel(r'Position in mm')
-plt.ylabel(r'Intensität in $\si{\micro}$A')
+plt.ylabel(r'Intensität in µA')
 plt.savefig("build/plot3.pdf")
 plt.clf()
 
 ############################################################
+
+plt.plot(x_fit1*1e3, theory(x_fit1, *params1)*1e6, 'b', label="Fit Einzelspalt")
+plt.plot(x_einzel*1e3, I_einzel*1e6, 'rx', label="Messwerte Einzelspalt")
+plt.grid()
+plt.xlim(9, 33)
+plt.xlabel(r'Position in mm')
+plt.ylabel(r'Intensität in µA')
+plt.plot(x_fit2*1e3-2.13, theory2(x_fit2, *params2)*1e6*1.092, 'k', label="Fit Doppelspalt")
+plt.plot(x_doppel1*1e3-2.13, I_doppel1*1e6*1.092, 'gx', label="Messwerte Doppelspalt")
+plt.legend()
+plt.savefig("build/plot4.pdf")
